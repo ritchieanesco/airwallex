6c06c4679cb2bfee5728fae56ff5ebdbb0afb7b7
ritchieanesco <info@ritchieanesco.com>
2018-06-03 11:59:01 +1000

    Fix linting error

--- src/Utils/form.js
+++ src/Utils/form.js
@@ -1,5 +1,7 @@
 export const emailValidation = email => {
+  /*eslint-disable*/
   const re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  /*eslint-enable*/
   return email !== "" && re.test(email);
 };
 
